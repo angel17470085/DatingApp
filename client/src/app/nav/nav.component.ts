dd167ee535d2412ef752b2da31833971600b34aa
angel17470085 <17470085@campeche.tecnm.mx>
2023-04-08 21:09:18 -0600

    Section 6:vid 67 - Adding a toast service for notifications toast version is 14, others make an error.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -13,7 +14,11 @@ export class NavComponent implements OnInit {
 
   model : any ={}
   
-  constructor( public accountService : AccountService , private router : Router) { }
+  constructor( 
+    public accountService : AccountService , 
+    private router : Router,
+    private toastr: ToastrService
+    ) { }
 
   
   ngOnInit(): void {
@@ -28,12 +33,10 @@ export class NavComponent implements OnInit {
 
   login()
   {
-    this.accountService.login(this.model).subscribe(
-      {
+    this.accountService.login(this.model).subscribe({
         next: () => this.router.navigateByUrl('/members'),
-        error:(error) => console.log(error)
-      }
-    );
+        error:(error) => this.toastr.error(error.error)
+      })
   }
 
   logout() {
