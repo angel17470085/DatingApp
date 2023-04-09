9d0f97300a765d4cbe5cf213b72b8d714e1e37a6
angel17470085 <17470085@campeche.tecnm.mx>
2023-04-08 19:20:40 -0600

    Section 6: vid 66 - routing in code, changing login and log out behaviour.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,7 @@ export class NavComponent implements OnInit {
 
   model : any ={}
   
-  constructor( public accountService : AccountService) { }
+  constructor( public accountService : AccountService , private router : Router) { }
 
   
   ngOnInit(): void {
@@ -29,10 +30,7 @@ export class NavComponent implements OnInit {
   {
     this.accountService.login(this.model).subscribe(
       {
-        next: (response) => {
-          console.log(response);
-        
-        },
+        next: () => this.router.navigateByUrl('/members'),
         error:(error) => console.log(error)
       }
     );
@@ -41,6 +39,7 @@ export class NavComponent implements OnInit {
   logout() {
     
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
