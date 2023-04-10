c39c77358f3bd087ef12ce768a57ee31b554fb8f
angel17470085 <17470085@campeche.tecnm.mx>
2023-04-09 20:27:43 -0600

    Section 6 - vid 71 cleaning code from the app module to a shared module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
@@ -15,8 +15,8 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
-import { TitleCasePipe } from '@angular/common';
+
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
@@ -36,8 +36,8 @@ import { TitleCasePipe } from '@angular/common';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({positionClass: 'toast-bottom-right'})
+    SharedModule
+
   ],
   providers: [
   ],
