c843c23b1503c4a643a184ed7bd7628576610c99
angel17470085 <17470085@campeche.tecnm.mx>
2023-04-17 05:06:34 -0600

    Section 10 -  118 persisting the changes to the api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -38,6 +39,29 @@ namespace API.Controllers
             return user;
 
 
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser(MemberUpdateDto updateMemberDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            
+            var user = await  _userRepository.GetUserByUserNameAsync(username);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(updateMemberDto,user);
+
+            if (await _userRepository.SaveAllAsync())
+            {
+                return NoContent();
+            } 
+            
+            return BadRequest("Failed to update user...");
+           
         }
     }
 }
