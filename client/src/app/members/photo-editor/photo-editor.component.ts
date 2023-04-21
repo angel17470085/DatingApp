0037ef57f8ff8efa3b93cb2feec3b1cd58d5cce8
angel17470085 <17470085@campeche.tecnm.mx>
2023-04-21 01:38:24 -0600

    Section 11 - 131 adding a photo uploader

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,16 +1,65 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
+import {  FileUploader } from 'ng2-file-upload';
+import { environment } from 'src/environments/environment';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { take } from 'rxjs';
+
 
 @Component({
   selector: 'app-photo-editor',
   templateUrl: './photo-editor.component.html',
   styleUrls: ['./photo-editor.component.css']
 })
+
 export class PhotoEditorComponent implements OnInit {
   @Input() member : Member | undefined;
-  constructor() { }
+  public uploader : FileUploader | undefined;
+  public hasBaseDropZoneOver : boolean = false;
+  baseUrl = environment.apiUrl;
+  user : User | undefined;
+ 
+  constructor(private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next : user => {
+        if (user) this.user = user;
+      }
+    })
+   }
+
 
   ngOnInit(): void {
+    this.initilizeUploader();
   }
+
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initilizeUploader ()
+  {
+      this.uploader = new FileUploader({
+      url : this.baseUrl + "users/add-photo",
+      authToken: "Bearer "+ this.user?.token,
+      isHTML5: true,
+      allowedFileType:["image"],
+      removeAfterUpload: true,
+      autoUpload : false,
+      maxFileSize: 10 * 1024 * 1024,
+    })
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member?.photos.push(photo);
+      }
+    }
+  }
+
 
 }
