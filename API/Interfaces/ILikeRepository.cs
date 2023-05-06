c268d5d78c9c00c3d4a4982610f202e66468907a
JoseAngel <17470085@campeche.tecnm.mx>
2023-05-06 17:31:57 -0600

    Section 14 - Adding a likes repository

---
+++ API/Interfaces/ILikeRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface ILikeRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+        
+        Task<AppUser> GetUserWithLikes(int userId);
+
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+
+    }
+}
