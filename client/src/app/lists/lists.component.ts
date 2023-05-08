2b79c587d40baacf5d8b26002abf9ed01ea29dfc
angel <17470085@campeche.tecnm.mx>
2023-05-08 03:59:10 -0600

    Section 14 - adding the likes component

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
+import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.css']
 })
+
+
 export class ListsComponent implements OnInit {
 
-  constructor() { }
+  members : Member[] | undefined;
+  predicate = "liked";
+
+
+  constructor(private memberService : MembersService) { }
+
 
   ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  loadLikes() {
+    this.memberService.getLikes(this.predicate).subscribe({
+      next : response => {
+        this.members = response;
+      }
+    });
   }
 
 }
