f666d13937262cb6603ec7af84818334a46040f0
angel <17470085@campeche.tecnm.mx>
2023-05-16 22:33:15 -0600

    Section 16 - setting up the entities

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,14 +1,10 @@
 using API.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities
 {
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-
         public DateOnly DateOfBirth { get; set; }
 
         public string KnownAs {get; set;} 
@@ -38,7 +34,7 @@ namespace API.Entities
     
         public List<Message> MessageReceived {get; set;}
         
-
+        public ICollection<AppUserRole> UserRoles { get; set; }
 
     }
 }
