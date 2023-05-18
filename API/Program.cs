66933c0947234554e31bd98cc7cca183e9e35420
angel <17470085@campeche.tecnm.mx>
2023-05-17 18:31:28 -0600

    Section 16 - Adding Roles to the App

--- API/Program.cs
+++ API/Program.cs
@@ -34,8 +34,9 @@ app.MapControllers();
     {
         var context = services.GetRequiredService<DataContext>();
         var userManager = services.GetRequiredService<UserManager<AppUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
         await context.Database.MigrateAsync();
-        await Seed.SeedUsers(userManager);
+        await Seed.SeedUsers(userManager, roleManager);
     }
     catch (System.Exception ex)
     {
